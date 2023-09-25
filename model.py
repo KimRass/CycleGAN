2d66b3fffb70d2650ca65d1818db228a991510b5
KimRass <rmx1000@yonsei.ac.kr>
2023-09-25 22:06:13 +0900

    Con and trans conv bias=False

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # References:
     # https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
+    # https://machinelearningmastery.com/how-to-implement-pix2pix-gan-models-from-scratch-with-keras/
 
 import torch
 import torch.nn as nn
@@ -7,10 +8,10 @@ import torch.nn.functional as F
 
 
 class ConvNormRelu(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, relu=True):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, activ):
         super().__init__()
 
-        self.relu = relu
+        self.activ = activ
 
         self.conv = nn.Conv2d(
             in_channels,
@@ -19,14 +20,17 @@ class ConvNormRelu(nn.Module):
             stride=stride,
             padding=padding,
             padding_mode="reflect",
+            bias=False,
         )
-        self.norm = nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=True)
+        self.norm = nn.InstanceNorm2d(out_channels, affine=False, track_running_stats=False)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.norm(x)
-        if self.relu:
+        if self.activ == "relu":
             x = torch.relu(x)
+        elif self.activ == "tanh":
+            x = torch.tanh(x)
         return x
 
 
@@ -40,8 +44,12 @@ class ResidualBlock(nn.Module):
         # self.conv2 = nn.Conv2d(
         #     channels, channels, kernel_size=3, stride=1, padding=padding, padding_mode="reflect",
         # )
-        self.conv1 = ConvNormRelu(channels, channels, kernel_size=3, stride=1, padding=padding, relu=True)
-        self.conv2 = ConvNormRelu(channels, channels, kernel_size=3, stride=1, padding=padding, relu=True)
+        self.conv1 = ConvNormRelu(
+            channels, channels, kernel_size=3, stride=1, padding=padding, activ="relu",
+        )
+        self.conv2 = ConvNormRelu(
+            channels, channels, kernel_size=3, stride=1, padding=padding, activ="relu",
+        )
 
     def forward(self, x):
         return x + self.conv2(self.conv1(x))
@@ -58,8 +66,9 @@ class TransConvNormRelu(nn.Module):
             stride=2,
             padding=padding,
             output_padding=output_padding,
+            bias=False,
         )
-        self.norm = nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=True)
+        self.norm = nn.InstanceNorm2d(out_channels, affine=False, track_running_stats=False)
 
     def forward(self, x):
         x = self.conv(x)
@@ -71,7 +80,7 @@ class TransConvNormRelu(nn.Module):
 # "Weights are initialized from a Gaussian distribution $N(0, 0.02).$"
 def _init_weights(model):
     for m in model.modules():
-        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.InstanceNorm2d)):
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
             m.weight.data.normal_(0, 0.02)
 
 
@@ -88,20 +97,32 @@ def _init_weights(model):
 # "The network with 9 residual blocks consists of: 'c7s1-64, d128, d256, R256, R256, R256, R256, R256,
 # R256, R256, R256, R256, u128, u64, c7s1-3'."
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, n_resid_blocks=9):
         super().__init__()
 
-        self.conv_block1 = ConvNormRelu(3, 64, kernel_size=7, stride=1, padding=3, relu=True) # "'c7s1-64'"
-        self.conv_block2 = ConvNormRelu(64, 128, kernel_size=3, stride=2, padding=1, relu=True) # "'d128'"
-        self.conv_block3 = ConvNormRelu(128, 256, kernel_size=3, stride=2, padding=1, relu=True) # "'d256'"
-        self.resid_blocks = nn.Sequential(*[ResidualBlock() for _ in range(1, 9 + 1)]) # "'R256'"
+        self.conv_block1 = ConvNormRelu(
+            3, 64, kernel_size=7, stride=1, padding=3, activ="relu",
+        ) # "'c7s1-64'"
+        self.conv_block2 = ConvNormRelu(
+            64, 128, kernel_size=3, stride=2, padding=1, activ="relu",
+        ) # "'d128'"
+        self.conv_block3 = ConvNormRelu(
+            128, 256, kernel_size=3, stride=2, padding=1, activ="relu",
+        ) # "'d256'"
+        self.resid_blocks = nn.Sequential(
+            *[ResidualBlock() for _ in range(n_resid_blocks)]
+        ) # "'R256'"
         self.trans_conv_block1 = TransConvNormRelu(
             256, 128, padding=1, output_padding=1,
         ) # "'u128'"
         self.trans_conv_block2 = TransConvNormRelu(
             128, 64, padding=1, output_padding=1,
         ) # "'u64'"
-        self.conv_block4 = ConvNormRelu(64, 3, kernel_size=7, stride=1, padding=3, relu=False) # "'c7s1-3'"
+        # 논문에는 나와있지 않지만, $[-1, 1]$의 tensor를 이미지로 변환할 것이므로 activation function으로 tanh를
+        # 사용하겠습니다.
+        self.conv_block4 = ConvNormRelu(
+            64, 3, kernel_size=7, stride=1, padding=3, activ="tanh",
+        ) # "'c7s1-3'"
 
         _init_weights(self)
 
@@ -117,29 +138,28 @@ class Generator(nn.Module):
 
 
 class ConvBlock(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        instance_norm=True,
-    ):
+    def __init__(self, in_channels, out_channels, norm=True):
         super().__init__()
 
-        self.instance_norm = instance_norm
+        self.norm = norm
 
         self.conv = nn.Conv2d(
             in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False,
         )
-        if instance_norm:
-            self.norm = nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=False)
+        if norm:
+            self.norm = nn.InstanceNorm2d(out_channels, affine=False, track_running_stats=False)
 
     def forward(self, x):
         x = self.conv(x)
-        if self.instance_norm:
+        if self.norm:
             x = self.norm(x)
         # "We use leaky ReLUs with a slope of 0.2."
-        x = F.leaky_relu(x, 0.2)
+        x = F.leaky_relu(x, slope=0.2)
         return x
+
+
+def get_receptive_field(out_channels, kernel_size, stride):
+    return (out_channels - 1) * stride + kernel_size
 
 
 # "We use 70 × 70 PatchGAN. Let 'Ck' denote a 4 × 4 Convolution-InstanceNorm-LeakyReLU layer
@@ -149,10 +169,10 @@ class Discriminator(nn.Module):
         super().__init__()
 
         # "We do not use InstanceNorm for the first 'C64' layer."
-        self.conv_block1 = ConvBlock(3, 64, instance_norm=False) # "'C64'"
-        self.conv_block2 = ConvBlock(64, 128, instance_norm=True) # "'C128'"
-        self.conv_block3 = ConvBlock(128, 256, instance_norm=True) # "'C256'"
-        self.conv_block4 = ConvBlock(256, 512, instance_norm=True) # "'C512'"
+        self.conv_block1 = ConvBlock(3, 64, norm=False) # "'C64'"
+        self.conv_block2 = ConvBlock(64, 128, norm=True) # "'C128'"
+        self.conv_block3 = ConvBlock(128, 256, norm=True) # "'C256'"
+        self.conv_block4 = ConvBlock(256, 512, norm=True) # "'C512'"
         # "After the last layer, we apply a convolution to produce a 1-dimensional output."
         self.conv_block5 = nn.Conv2d(512, 1, kernel_size=1)
 
