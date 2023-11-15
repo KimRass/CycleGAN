1ab79d59c758c2111fabb313aa181c311c74cece
KimRass <rmx1000@yonsei.ac.kr>
2023-11-16 00:49:18 +0900

    No deterministric algorithms

--- utils.py
+++ utils.py
@@ -27,7 +27,6 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
-    torch.use_deterministic_algorithms(True)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
